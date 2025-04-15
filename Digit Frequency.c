ed9acc283ca6cbed7fd9e03d9a15ac8c89c46173
FEBIN <117272878+febin-ch@users.noreply.github.com>
2025-04-15 21:53:05 +0530

    Create Digit Frequency.c

---
+++ Digit Frequency.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char sentence[100];
+    int zero=0,one=0,two=0,three=0,four=0,five=0,six=0,seven=0,eight=0,nine=0;
+    fgets(sentence,sizeof(sentence),stdin);
+    for(int i=0;sentence[i] != '\0';i++)
+    {
+        if(sentence[i]=='0')
+        {
+            zero++;
+        }
+        else if(sentence[i]=='1')
+        {
+            one=one+1;
+        }
+        else if(sentence[i]=='2')
+        {
+            two++;
+        }
+        else if(sentence[i]=='3')
+        {
+            three++;
+        }
+        else if(sentence[i]=='4')
+        {
+            four++;
+        }
+        else if(sentence[i]=='5')
+        {
+            five++;
+        }
+        else if(sentence[i]=='6')
+        {
+            six++;
+        }
+        else if(sentence[i]=='7')
+        {
+            seven++;
+        }
+        else if(sentence[i]=='8')
+        {
+            eight++;
+        }
+        else if(sentence[i]=='9')
+        {
+            nine++;
+        }
+    
+}
+printf("%d %d %d %d %d %d %d %d %d %d",zero,one,two,three,four,five,six,seven,eight,nine);
+}
